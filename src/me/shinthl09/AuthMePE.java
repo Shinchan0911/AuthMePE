ed7930c3a7e3151b195c33ebc73d9b9680a70046
Shinchan0911 <98627396+Shinchan0911@users.noreply.github.com>
2022-09-03 23:36:15 +0700

    Update AuthMePE.java

--- src/me/shinthl09/AuthMePE.java
+++ src/me/shinthl09/AuthMePE.java
@@ -16,35 +16,36 @@ public class AuthMePE extends JavaPlugin {
 		Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
 		getConfig().options().copyDefaults(true);
 		saveDefaultConfig();
-		getLogger().log(Level.INFO,color.transalate("&e---------------------"));
-		getLogger().log(Level.INFO,color.transalate(""));
-		getLogger().log(Level.INFO,color.transalate("&aAuthor:&e ShinTHL09"));
-		getLogger().log(Level.INFO,color.transalate("&aPlugin:&e Has Enable"));
-		getLogger().log(Level.INFO,color.transalate(""));
-		getLogger().log(Level.INFO,color.transalate("&e---------------------"));
+		getLogger().log(Level.INFO, color.transalate("&e---------------------"));
+		getLogger().log(Level.INFO, color.transalate(""));
+		getLogger().log(Level.INFO, color.transalate("&aAuthor:&e ShinTHL09"));
+		getLogger().log(Level.INFO, color.transalate("&aPlugin:&e Has Enable"));
+		getLogger().log(Level.INFO, color.transalate(""));
+		getLogger().log(Level.INFO, color.transalate("&e---------------------"));
 	}
+
 	@Override
 	public void onDisable() {
-		getLogger().log(Level.INFO,color.transalate("&e---------------------"));
-		getLogger().log(Level.INFO,color.transalate(""));
-		getLogger().log(Level.INFO,color.transalate("&aAuthor:&e ShinTHL09"));
-		getLogger().log(Level.INFO,color.transalate("&aPlugin:&e Has Disable"));
-		getLogger().log(Level.INFO,color.transalate(""));
-		getLogger().log(Level.INFO,color.transalate("&e---------------------"));
+		getLogger().log(Level.INFO, color.transalate("&e---------------------"));
+		getLogger().log(Level.INFO, color.transalate(""));
+		getLogger().log(Level.INFO, color.transalate("&aAuthor:&e ShinTHL09"));
+		getLogger().log(Level.INFO, color.transalate("&aPlugin:&e Has Disable"));
+		getLogger().log(Level.INFO, color.transalate(""));
+		getLogger().log(Level.INFO, color.transalate("&e---------------------"));
 	}
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("authmepe") && sender.hasPermission("authmepe.admin")) {
 			if (args.length == 0) {
 				String prefix = color.transalate(this.getConfig().getString("Prefix"));
 				sender.sendMessage(prefix + color.transalate(this.getConfig().getString("Command-Wrong")));
+			} else if (args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("rl")) {
+				String prefix = color.transalate(this.getConfig().getString("Prefix"));
+				sender.sendMessage(prefix + color.transalate(this.getConfig().getString("No-Permissions")));
 			} else {
 				String prefix = color.transalate(this.getConfig().getString("Prefix"));
-				reloadConfig();
-				sender.sendMessage(prefix + color.transalate(this.getConfig().getString("Reload-Successful")));
+				sender.sendMessage(prefix + color.transalate(this.getConfig().getString("Command-Wrong")));
 			}
-		} else {
-			String prefix = color.transalate(this.getConfig().getString("Prefix"));
-			sender.sendMessage(prefix + color.transalate(this.getConfig().getString("No-Permissions")));
 		}
 		return true;
 	}
