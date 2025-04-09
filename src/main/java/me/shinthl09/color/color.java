c26b4280611217f15fe0a5c4637a18f22fa39911
Shinchan0911 <truonghuuloc212@gmail.com>
2025-04-09 21:56:29 +0700

    support hex color

--- src/main/java/me/shinthl09/color/color.java
+++ src/main/java/me/shinthl09/color/color.java
@@ -4,10 +4,28 @@ import me.shinthl09.AuthMePE;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class color {
     private static Plugin plugin = AuthMePE.getPlugin(AuthMePE.class);
-    public static String transalate(String message) {
-        return ChatColor.translateAlternateColorCodes('&', (message.replace("{prefix}", plugin.getConfig().getString("Setting.Prefix"))));
 
+    public static String transalate(String message) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder("");
+            for (char c : ch) {
+                builder.append("&" + c);
+            }
+
+            message = message.replace(hexCode, builder.toString());
+            matcher = pattern.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', (message.replace("{prefix}", plugin.getConfig().getString("Setting.Prefix"))));
     }
 }
